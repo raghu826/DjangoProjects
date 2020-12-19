59d1cfdeb686c2b1ad5d725c67484af333fe488a
Your Name <yraghu13@gmail.com>
2020-12-19 23:10:27 +0100

    first commit

---
+++ portfolio/myProjects/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from myProjects.models import Project
+
+def project_index(request):
+    myProjects = Project.objects.all()
+    context = {
+        'myProjects': myProjects
+    }
+    return render(request, 'project_index.html', context)
+
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
